01f60b8c53b6ed97b756de469ddbdb9a69ddb560
16519413 M. Rafli Zamzami <kiipliwooke@gmail.com>
2020-11-15 00:40:26 +0700

    searching template sama save document

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
 import os
+from docs_processing import get_similar_articles
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/Algeo02-19039/src'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -9,7 +14,19 @@ def index():
 
 @app.route('/search',methods=['POST'])
 def search():
-    return render_template('index.html')
+    query = request.form['query']
+    
+    file=request.files['file']
+    filename=secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+    
+    titles =['uzumaki bayu','uzumaki saburo']
+    doc_url=['kipli.txt','kipli2.txt']
+    desc=['ini isinya kalimat pertama','ini isinya kalimat kedua']
+    word_count=['203','102']
+    cos_sim=['0.709123','0.02312']
+    return render_template('result.html', query=query, titles=titles, doc_url=doc_url, desc=desc, word_count=word_count, cos_sim=cos_sim)
+
 
 @app.route('/upload',methods=['POST'])
 def upload():
